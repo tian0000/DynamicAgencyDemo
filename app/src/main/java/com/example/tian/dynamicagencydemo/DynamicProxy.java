6548c4b0ca1a4a8898fc679abfee8805bea92f64
tian0000 <1173568715@qq.com>
2017-09-08 21:32:01 +0800

    werfsd

--- app/src/main/java/com/example/tian/dynamicagencydemo/DynamicProxy.java
+++ app/src/main/java/com/example/tian/dynamicagencydemo/DynamicProxy.java
@@ -23,12 +23,12 @@ public class DynamicProxy implements InvocationHandler {
     @Override
     public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
 //      在代理真实对象前我们可以添加一些自己的操作
-        System.out.println("代理真实对象前的操作");
-        System.out.println("Method:" + method);
+//        System.out.println("代理真实对象前的操作");
+//        System.out.println("Method:" + method);
         //    当代理对象调用真实对象的方法时，其会自动的跳转到代理对象关联的handler对象的invoke方法来进行调用
         method.invoke(subject, objects);
         //      在代理真实对象后我们也可以添加一些自己的操作
-        System.out.println("代理真实对象后的操作");
+//        System.out.println("代理真实对象后的操作");
         return null;
     }
 }
